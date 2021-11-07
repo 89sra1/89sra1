c198e3c421e153355a6bb493fa720b05812a39fd
SKUMAR25 <sravan.maddenapally@volvocars.com>
2021-11-07 22:01:58 +0100

    added context manager

--- Week4/Sravan_Assignment4.py
+++ Week4/Sravan_Assignment4.py
@@ -10,11 +10,22 @@ def read_list():
 
 
 def main():
-    try:
-        text_list = read_list()
-        print(sys.getsizeof(text_list), "Bytes are used by the list")
-    except Exception:
-        print("something is wrong")
+    class MyFileContextManager():
+        def __init__(self, filename, operation):
+            self._file = open(filename, operation)
+
+        def __enter__(self):
+            print("file is opened and fetched")
+            return self._file
+
+        def __exit__(self):
+            print("closing the file")
+            self._file.close()
+
+    with MyFileContextManager("FILENAME", "r") as f:
+        text = f.read()
+        print("file is done with the operation given by user")
+    print(sys.getsizeof(text), "Bytes are used by the list")
 
 
 if __name__ == "__main__":
